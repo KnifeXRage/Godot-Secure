7206d5b1eca7158c90c97830d0d074afb643229c
Aditya Raj <knifexofficial@gmail.com>
2025-07-31 17:54:14 +0530

    Revert Changes

--- Godot Secure Camellia-256.py
+++ Godot Secure Camellia-256.py
@@ -54,18 +54,17 @@ token_c_array = ', '.join([f'0x{b:02X}' for b in security_token])
 baseHeader = generate_magic_header(baseTag)
 encHeader = generate_magic_header(encTag)
 fileCreated = True
-backup_path = None
 
 MODIFICATIONS = [
     #Pre -Steps:
     {
-        "file": "version.py",
+        "file": "editor/editor_node.cpp",
         "operations": [
             {
                 "type": "replace_line",
                 "description": "Modify Godot title To add Godot Secure",
-                "find": "name = \"Godot Engine\"",
-                "replace": "name = \"Godot Engine (With Godot Secure)\""
+                "find": "DisplayServer::get_singleton()->window_set_title(title + String(\" - \") + GODOT_VERSION_NAME);",
+                "replace": "DisplayServer::get_singleton()->window_set_title(title + String(\" - \") + GODOT_VERSION_NAME + String(\" (With Godot Secure)\"));"
             }
         ]
     },
@@ -487,10 +486,7 @@ if __name__ == "__main__":
     else:
         # Too many arguments provided
         print("\nUsage: python Godot_Secure.py <godot_source_root>")
-        try:
-            exit = input("\nPress Enter key to exit...")
-        except EOFError:
-            pass
+        exit = input("\nPress Enter key to exit...")
         sys.exit(1)
         
 
@@ -500,20 +496,14 @@ if __name__ == "__main__":
 
     if not (os.path.isdir(core_dir) and os.path.isfile(sconstruct_file)):
         print(f"{LogColors.FAIL}Error: No valid Godot Source Detected in the Specified Directory.{LogColors.ENDC}")
-        try:
-            exit = input("\nPress Enter key to exit...")
-        except EOFError:
-            pass
+        exit = input("\nPress Enter key to exit...")
         sys.exit(1)
 
     print(f"\nUsing Godot Source Root: {godot_root}")
     confirm = input(f"\n\n ⚠   {LogColors.WARNING}Start Godot Secure Operations on Godot Source Root {LogColors.ENDC}{LogColors.FAIL}(y/n)?{LogColors.ENDC}: ").strip().lower()
     if not (confirm == 'y' or confirm == 'yes'):
         print("Closing Setup...")
-        try:
-            exit = input("\nPress Enter key to exit...")
-        except EOFError:
-            pass
+        exit = input("\nPress Enter key to exit...")
         sys.exit(1)
         
     print(f"\n\n{LogColors.HEADER}=== Applying Camellia Encryption For Godot ==={LogColors.ENDC}")
@@ -523,11 +513,7 @@ if __name__ == "__main__":
         print(f"{LogColors.BOLD} Security Token:{LogColors.ENDC} {token_hex}")
         print_warning(f"{LogColors.WARNING} Keep this token secret - it's required for decryption!{LogColors.ENDC}")
         print_success(f"{LogColors.OKGREEN} Build is now cryptographically unique{LogColors.ENDC}")
-        if not (backup_path == None):
-            print_info(f"{LogColors.OKGREEN} Old Key Backup created at: {LogColors.ENDC}{LogColors.BOLD}{backup_path}{LogColors.ENDC}\n")
+        print_info(f"{LogColors.OKGREEN} Old Key Backup created at: {LogColors.ENDC}{LogColors.BOLD}{backup_path}{LogColors.ENDC}\n")
     
-    try:
-        exit = input("\nPress Enter key to exit...")
-    except EOFError:
-        pass
+    exit = input("\nPress Enter key to exit...")
     sys.exit(1)
